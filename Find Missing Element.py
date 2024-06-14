fa7394711b3cb320e19c4d6774e224f1097b8763
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-06-14 13:56:46 +0100

    The beauty of Data Structures & Algorithms - A simple code block to find the contrast between arrays.

--- Find Missing Element.py
+++ Find Missing Element.py
@@ -7,34 +7,40 @@
 import numpy as np
 
 
-def make_array(arr):  # taking parameters array and k, k being the deleted element.
+def make_array(arr):
 
-    b_arr = np.array(arr)
-    np.random.shuffle(b_arr)
+    b_arr = np.array(arr)  # initialize array to numpy array.
+    np.random.shuffle(b_arr)  # shuffle the array
 
-    random_index = np.random.randint(0, len(b_arr))
-    b_arr = np.delete(b_arr, random_index)
+    # delete a random element and return the "new" array.
+    random_index = np.random.randint(0, len(b_arr))  # choosing a random integer from the array.
+    b_arr = np.delete(b_arr, random_index)  # delete the random integer and attach the new array to variable "b_arr"
     return b_arr
 
 
 def contrast_between_arrays(arr_1, arr_2):
+    # sorting both arrays.
     a1 = sorted(arr_1)
     a2 = sorted(arr_2)
-    seen = {}
+    seen = {}  # create a dictionary for storing seen elements in both array.
     for element in a1:
         if element in seen:
             seen[element] += 1
 
         else:
+            # if element not in seen, append to "seen".
             seen[element] = 1
 
     for element in a2:
+        # if element in seen, "displace" from "seen".
         if element in seen:
             seen[element] -= 1
         else:
             seen[element] = 1
 
     for k in seen:
+        # if the difference in elements between the first and second arrays does not equal 0, return k.
+        # "k" being the outlier element.
         if seen[k] != 0:
             return k
     return "No missing elements"
