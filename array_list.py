60b10a7ecc07b6a5f4b93ac1f544e1fcf1c6bcd7
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-06-20 13:06:27 +0100

    The beauty of Data Structures & Algorithms - Array list - a module to store python list to a file

--- array_list.py
+++ array_list.py
@@ -6,10 +6,11 @@ array_elements = input("array list elements (seperated by commas): ")
 array_list = array_elements.split(",")
 
 with open("list.json", "w") as file:
-    for x in array_list:
-        json.dump(int(x), file)
+        json.dump([int(x) for x in array_list], file)
+
 
 with open("list.json", "r") as file:
     arr = json.load(file)
 
 print(arr)
+
