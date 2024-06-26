acc77e6332e36f61b7c2a8214532c9a04c12cb40
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-06-26 12:38:54 +0100

    The beauty of Data Structures & Algorithms - Rename Notebook - Beauty of Algorithms : Sentence Reversal Problem

--- Sentence Reversal.py
+++ Sentence Reversal.py
@@ -1,1 +1,14 @@
 # simple code block to reverse an inputted string or combination of strings.
+
+def reverse(sentence):
+    white_space = " "
+    sentence_segmented = sentence.split(white_space)  # seperate the sentence into segments via white spaces.
+
+    rev = sentence_segmented[::-1]  # returns segments starting from the last index.
+    rev_sentence = " ".join(rev)  # segments are attached via white-space.
+    rev_sentence.strip(white_space)  # remove trailing and preceeding white spaces.
+    return rev_sentence
+
+sentence = str(input("sentence to be reversed: "))
+print(sentence)
+print(reverse(sentence))
