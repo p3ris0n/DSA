6f6624706e2524dcffee3ca02775e8861ecf76bd
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-06-28 14:43:02 +0100

    The beauty of Data Structures & Algorithms - Rename Notebook - Beauty of Algorithms : Sentence Reversal Problem

--- Sentence Reversal.py
+++ Sentence Reversal.py
@@ -1,5 +1,8 @@
 # simple code block to reverse an inputted string or combination of strings.
 
+import json
+#import array_list as al
+
 def reverse(sentence):
     white_space = " "
     sentence_segmented = sentence.split(white_space)  # seperate the sentence into segments via white spaces.
@@ -9,6 +12,13 @@ def reverse(sentence):
     rev_sentence = rev_sentence.strip(white_space)  # remove trailing and preceeding white spaces.
     return rev_sentence
 
-sentence = str(input("sentence to be reversed: "))
-print(sentence)
+
+sentence = input("sentence to be reversed: ")
+listname = input("list name: ")
+data = {listname: sentence}
+with open("list.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+
 print(reverse(sentence))
+
